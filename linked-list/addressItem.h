9f0cf36185b4c36b9e5e141bcc231ab2d3dcdce9
Matthew Connelly <matthew.b.connelly@gmail.com>
2018-10-29 18:31:19 -0400

    init linked-list lab

---
+++ linked-list/addressItem.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class AddressItem{
+
+public:
+
+AddressItem ( ) {count = 0;}
+
+friend istream & operator>>(istream & in, AddressItem & a); //inputs one ip address into a
+
+friend ostream & operator<<(ostream & out, const AddressItem & a);// outputs ip and count
+
+void Tally( ); // add one to address item’s count
+
+string getAddress()const;
+
+int getCount()const;
+
+bool operator==(const AddressItem & addr2)const; //compare address values only
+
+private:
+
+string address;
+
+int count; 
+
+};
