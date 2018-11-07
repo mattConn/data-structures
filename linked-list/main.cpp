352121bfcd688e0f06dcc89c3231fc99430e2913
Matthew Connelly <matthew.b.connelly@gmail.com>
2018-11-06 20:29:05 -0500

    successful input of addresses into list

--- linked-list/main.cpp
+++ linked-list/main.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
+#include <fstream>
 #include "addressItem.h"
+#include "LinkedList.h"
 
 using namespace std;
 
-void copyLines();
 
 int main()
 {
+    fstream inFile;
+    inFile.open("addresses.txt");
+
+    LinkedList<AddressItem> list;
+
+    while(!inFile.eof())
+    {
+        AddressItem a;
+        inFile >> a;
+        list.insert(list.getLength()+1,a);
+    }
+    for(int i = 0; i < list.getLength(); i++)
+        cout << list.getEntry(i+1).getAddress() << endl;
+
     AddressItem a;
 
-    cin >> a;
+    inFile.close();
+
     return 0;
 }
