3865976ca36f05ff349c7929f6f28da3582e0b4d
Matthew Connelly <matthew.b.connelly@gmail.com>
2018-11-07 14:53:55 -0500

    linked-list additional comments

--- linked-list/main.cpp
+++ linked-list/main.cpp
@@ -17,6 +17,7 @@ int main()
     // copy addresses to list
     while(!inFile.eof())
     {
+        // store address, tally address
         AddressItem a;
         inFile >> a;
         a.Tally();
@@ -28,30 +29,32 @@ int main()
         {
             for(int i = 1; i < list.getLength()+1; i++)
             {
-                if(list.getEntry(i) == a)
+                if(list.getEntry(i) == a) // duplicate found
                 {
-                    // tally
+                    // tally (copy count of existing address)
                     for(int j = 0; j < list.getEntry(i).getCount(); j++)
                         a.Tally();
 
-                    // using replace because getEntry returns a value (deref. nodePtr)
+                    // replace existing address with current address (save count)
+                    // note: using replace because getEntry returns a value (deref. nodePtr)
                     list.replace(i,a);
-                    dup = true;
-                    break;
+                    dup = true; // set duplicate flag
+                    break; // exit for loop
                 }
             }
         }
 
-        // if not a duplicate, insert
+        // if not a duplicate, insert (new entry)
         if(!dup)
             list.insert(list.getLength()+1,a);
     }
 
-    // print list
-    string printHeader = "IP Address : Occurences";
+    // list header for display
+    string printHeader = "IP Address : Occurences"; // string obj. for length
     cout << printHeader << endl;
-    cout << string(printHeader.length(),'=') << endl;
+    cout << string(printHeader.length(),'=') << endl; // using string length here
 
+    // print list
     for(int i = 1; i < list.getLength(); i++)
         cout << list.getEntry(i);
 
