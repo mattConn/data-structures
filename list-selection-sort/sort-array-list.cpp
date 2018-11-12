1595579d373ee2b34172fb93669fa5386915b213
Matthew Connelly <matthew.b.connelly@gmail.com>
2018-11-12 17:15:14 -0500

    addition of list printing function

--- list-selection-sort/sort-array-list.cpp
+++ list-selection-sort/sort-array-list.cpp
@@ -1,22 +1,56 @@
 #include <iostream>
 #include <string>
 #include "array-list/ArrayList.h"
-//PrecondViolatedExcep::PrecondViolatedExcep(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > const&)'
 
 using namespace std;
 
 // copy file contents to list obj.
-void fileToList(const string &fileName, ArrayList<int> &list);
+template<class T>
+void fileToList(const string &fileName, ArrayList<T> &list);
+
+// print items in list
+template<class T>
+void printList(const ArrayList<T> &list);
 
 int main()
 {
-	ArrayList<int> a;
+	ArrayList<int> list;
+
+	printList(list);
+	cout << string(10,'=') << endl;
+	
+	list.insert(list.getLength()+1, 13);
+	
+	printList(list);
+	cout << string(10,'=') << endl;
+
+	list.insert(list.getLength()+1, 74);
+	
+	printList(list);
+	cout << string(10,'=') << endl;
 
 	
 	
 	return 0;
 }
 
-void fileToList(const string &fileName, ArrayList<int> &list)
+template<class T>
+void fileToList(const string &fileName, ArrayList<T> &list)
 {
+}
+
+template<class T>
+void printList(const ArrayList<T> &list)
+{
+	if(list.getLength() == 0)
+	{
+		cout << "List is empty." << endl;
+		return;
+	}
+
+	int i = 0;
+	for(; i < list.getLength()-1; i++)
+		cout << list.getEntry(i+1) << ", ";
+
+	cout << list.getEntry(i+1) << endl;
 }
